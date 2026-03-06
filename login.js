d9d948fa84357544ea6f14d7dcdb07d6f7c7dfac
AhmadZaid20187 <ahmadzaid20187@gmail.com>
2026-03-07 01:16:39 +0600

    Login page and Navigate to Issu Page has Done

---
+++ login.js
@@ -0,0 +1,15 @@
+const loginBtn = document.getElementById("loginBtn").addEventListener("click", function () {
+    const inputUsername = document.getElementById("inputUsername")
+    const userName = inputUsername.value;
+    const inputPassword = document.getElementById("password")
+    const password = inputPassword.value;
+    console.log(userName)
+    console.log(password)
+    if (userName == "admin" && password == "admin123") {
+        alert("Login Successfull")
+        window.location.assign("/issu.html")
+    } else {
+        alert("Login Faild")
+        return;
+    }
+})
