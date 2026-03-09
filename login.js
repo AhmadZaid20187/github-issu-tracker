a2382543414908100e958feaabbbd16a3b3fc429
AhmadZaid20187 <ahmadzaid20187@gmail.com>
2026-03-09 17:57:25 +0600

    live link test

--- login.js
+++ login.js
@@ -7,7 +7,7 @@ const loginBtn = document.getElementById("loginBtn").addEventListener("click", f
     console.log(password)
     if (userName == "admin" && password == "admin123") {
         alert("Login Successfull")
-        window.location.assign("/issu.html")
+        window.location.assign("home.html")
     } else {
         alert("Login Faild")
         return;
--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 const issuesContainer = document.getElementById("issuesContainer")
+const loadingSpinner = document.getElementById("loadingSpinner")
+const openBtn = document.getElementById("open")
 
+function showLoading() {
+    loadingSpinner.classList.remove("hidden");
+    loadingSpinner.classList.add("flex");
+    issuesContainer.innerHTML = "";
+}
+function hideLoading() {
+    loadingSpinner.classList.add("hidden")
+    loadingSpinner.classList.remove("flex")
+}
 
-
-
-// async function loadIssues() {
-//     const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
-//     const info = await res.json();
-//     console.log(info)
-//     info.data.forEach(issues => {
-//         console.log(issues)
-
-
-
-//         // BTN for test
-
-//         // const btn = document.createElement("button")
-//         // btn.className = "btn btn-primary px-9";
-//         // btn.textContent = issues.status
-//         // openClosedBtn.appendChild(btn)
-//     });
-//     // openClosedBtn.innerHTML = `button will be in there`
-// }
+async function opendBtn() {
+    showLoading();
+    const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+    const openInfo = await res.json();
+    hideLoading();
+    displayOpenIssues(openInfo.data);
+}
 
 async function loadIssues() {
+    showLoading();
     const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
     const info = await res.json();
-    displayIssues(info.data)
+    hideLoading();
+    displayIssues(info.data);
 }
 
 function displayIssues(issues) {
@@ -63,9 +63,43 @@ function displayIssues(issues) {
                 </div>
         `;
         issuesContainer.appendChild(card)
-
-
     });
 }
 
+function displayOpenIssues(issues) {
+    issues.array.forEach(issu => {
+        const cardO = document.createElement("div");
+        cardO.className = "card bg-base-100 shadow-sm";
+        cardO.innerHTML =
+            `
+        <div class="card-body space-y-2">
+                    <div class="flex justify-between">
+                        <img src="./assets/Open-Status.png" alt="">
+                            <h2 class="badge badge-soft badge-error px-6 rounded-full">${issu.priority}</h2>
+                    </div>
+                    <h2 class="card-title">
+                        ${issu.title}
+                    </h2>
+                    <p class="line-clamp-2">${issu.description}</p>
+                    <div class="card-actions justify-start">
+                        <div class="badge  badge-error"><i class="fa-solid fa-bug"></i>${issu.labels[0]}</div>
+                        <div class="badge  badge-warning"><i class="fa-solid fa-circle-radiation"></i>${issu.labels[1]}</div>
+                        <div class="badge badge-accent">
+                            <i class="fa-solid fa-wand-magic-sparkles"></i>Enhancement
+                        </div>
+
+                    </div>
+
+                    <hr>
+
+                        <div>
+                            <h1><span>#${issu.id}</span> ${issu.author}</h1>
+                            <p>${issu.updatedAt}</p>
+                        </div>
+                </div>
+        `
+        issuesContainer.appendChild(cardO)
+    });
+}
 loadIssues();
+opendBtn();
